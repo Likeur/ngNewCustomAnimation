519bbeaa8df2978f13fbbc7997ed1ab5884397ad
Likeur <jacksimba28@gmail.com>
2023-11-08 16:02:52 +0200

    ng new custom animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import SplitType from 'split-type';
+
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-root',
@@ -9,6 +15,95 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styles: [],
 })
-export class AppComponent {
-  title = 'new-ng-anim';
+export class AppComponent implements OnInit{
+  
+
+  ngOnInit(){
+    gsap.registerPlugin(ScrollTrigger);
+
+    const element = document.querySelector('.element-centrable');
+
+    gsap.to(element, {
+      y: '50%',
+      translateX: '-50%',
+      top: '30%',
+      left: '50%',
+      position: 'fixed',
+      scrollTrigger: {
+        trigger: element,
+        start: 'top center',
+        end: 'bottom bottom',
+        scrub: true
+      }
+    });
+
+    // animate words
+        gsap.from('.deplacer', {
+                  scrollTrigger: {
+                      trigger: '.deplacer',
+                      start: 'top 40%',
+                      end: 'top top',
+                      scrub: true,
+                    
+                  },
+                  x:-210,
+        })       
+        gsap.to('.disparait', {
+                  scrollTrigger: {
+                      trigger: '.disparait',
+                      start: 'top 30%',
+                      end: 'bottom top',
+                      scrub: true,
+                  
+
+                  },
+                  opacity: 0,
+                 
+        })             
+        gsap.to('.grandir', {
+                  scrollTrigger: {
+                      trigger: '.grandir',
+                      start: 'bottom 20%',
+                      end: 'bottom top',
+                      scrub: true,
+                     
+                  },
+                  
+                  scale: 15,
+                  duration:2,
+                  stagger: 0.1
+        })       
+        gsap.to('.tourner', {
+                  scrollTrigger: {
+                      trigger: '.tourner',
+                      start: 'top 10%',
+                      end: 'bottom top',
+                      scrub: true,
+                  },
+                  
+                  rotate: 180,
+                  duration:2,
+                  x:-10,
+                  borderRadius:0,
+                  stagger: 0.1,
+                  
+        })       
+        gsap.to('.hello-apparait', {
+                  scrollTrigger: {
+                      trigger: '.hello-apparait',
+                      start: 'top center',
+                      end: 'bottom top',
+                      scrub: true,
+                      pin:'.hello-apparait'
+                  },
+                  
+                  scale:2,
+                  // y: '50%',
+                  // // translateX: '-50%',
+                  // top: '30%',
+                  // left: '50%',
+                  // position: 'fixed',
+                  
+        })       
+  }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,ts}"],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [
+    
+  ],
+}
+
